ea3161177ec116fe9640631c52c3ab9ebf534498
Jonathan Steele <jonathan@steele.pro>
2019-09-19 13:21:49 +0100

    feat: Update Profile component to use graphcms-image, GraphCMS asset

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Img from 'gatsby-image'
+import Img from 'graphcms-image'
 import tw from 'tailwind.macro'
 
 const Header = tw.header`border-0 border-b-2 border-gray-200 border-solid flex flex-col md:flex-row items-center mb-6 md:mb-8 py-6 md:py-8`
@@ -9,7 +9,15 @@ const Title = tw.div`flex flex-col`
 function Profile({ avatar, children }) {
   return (
     <Header>
-      <StyledImg alt="Jonathan Steele" fixed={avatar} title="Jonathan Steele" />
+      <StyledImg
+        alt="Jonathan Steele"
+        image={avatar}
+        title="Jonathan Steele"
+        withWebP
+        style={{
+          width: 120,
+        }}
+      />
       <Title>{children}</Title>
     </Header>
   )
