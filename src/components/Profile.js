2700cf67c4e992118896064718d44d3b47d2e4dc
Jonathan Steele <jonathan@steele.pro>
2019-12-18 12:04:32 +0000

    feat: Use gatsby-image to render avatar asset

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import Img from 'graphcms-image'
+import Img from 'gatsby-image'
 
 function Profile({ avatar, children }) {
   return (
     <header className="border-0 border-b-2 border-gray-200 border-solid flex flex-col md:flex-row items-center mb-6 md:mb-8 py-6 md:py-8">
       <Img
-        className="mb-4 md:mb-0 md:mr-8"
+        fluid={avatar.childImageSharp.fluid}
+        className="mb-4 md:mb-0 md:mr-8 rounded-sm w-24"
         alt="Jonathan Steele"
-        image={avatar}
         title="Jonathan Steele"
-        withWebP
-        style={{
-          width: 120,
-        }}
       />
       <div className="flex flex-col">{children}</div>
     </header>
