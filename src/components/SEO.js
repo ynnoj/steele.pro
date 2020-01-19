99c930c6044e86c659202472ea3d78e44efac467
Jonathan Steele <jonathan@steele.pro>
2020-01-19 11:57:28 +0000

    feat: Adding more SEO details

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -7,12 +7,16 @@ function SEO({ pageDescription, pageTitle }) {
   const { description: siteDescription, title: siteTitle } = useSiteMetadata()
 
   return (
-    <Helmet
-      defaultTitle={siteTitle}
-      titleTemplate={`%s - ${siteTitle}`}
-      title={pageTitle}
-    >
+    <Helmet defaultTitle={siteTitle} titleTemplate={`%s - ${siteTitle}`}>
+      <html lang="en" amp />
+      <title>{pageTitle}</title>
       <meta name="description" content={pageDescription || siteDescription} />
+      <meta name="og:title" content={pageTitle} />
+      <meta
+        name="og:descriptipn"
+        content={pageDescription || siteDescription}
+      />
+      <meta name="og:type" content="website" />
     </Helmet>
   )
 }
