10594afc04796343490eac2238d3a6a0488a36f7
Jonathan Steele <jonathan@steele.pro>
2020-05-02 19:03:07 +0100

    feat: Add mobile navigation

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import Avatar from './Avatar'
+import CloseIcon from '../svg/close.svg'
+import MenuIcon from '../svg/menu.svg'
 import Nav from './Nav'
+import { useNavigation } from '../../context/navigation'
 
 function Header() {
+  const { navOpen, toggleNav } = useNavigation()
+
+  const Icon = navOpen ? CloseIcon : MenuIcon
+
   return (
     <header className="border-b-2 border-gray-200 border-solid items-center py-6 md:py-8">
-      <div className="flex items-center justify-between max-w-3xl mx-auto px-4">
-        <Avatar />
+      <div className="md:flex items-center justify-between max-w-3xl mx-auto px-4">
+        <div className="flex items-center justify-between">
+          <Avatar />
+          <button className="focus:outline-none md:hidden" onClick={toggleNav}>
+            <Icon
+              className={classnames('h-6 w-6', {
+                'text-gray-600': navOpen,
+              })}
+            />
+          </button>
+        </div>
         <Nav />
       </div>
     </header>
--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { graphql, Link, useStaticQuery } from 'gatsby'
+import classnames from 'classnames'
+
+import { useNavigation } from '../../context/navigation'
 
 function Nav() {
+  const { closeNav, navOpen } = useNavigation()
   const {
     allMdx: { nodes: pages },
   } = useStaticQuery(graphql`
@@ -18,14 +22,20 @@ function Nav() {
   `)
 
   return (
-    <nav>
-      <ul className="flex -mx-2">
+    <nav
+      className={classnames({
+        'hidden md:block': !navOpen,
+        block: navOpen,
+      })}
+    >
+      <ul className="md:flex mt-4 md:mt-0 -mx-2 md:-mx-4">
         {pages.map(({ frontmatter: { title } }, i) => (
-          <li className="mx-2" key={i}>
+          <li className="mx-2 md:mx-4" key={i}>
             <Link
               to={`/${title.toLowerCase()}`}
               activeClassName="text-gray-900"
-              className="hover:text-gray-900 py-1 text-gray-600 text-lg"
+              className="block hover:bg-gray-100 md:hover:bg-transparent hover:text-gray-900 text-gray-600 text-lg px-4 py-2 md:p-0"
+              onClick={closeNav}
             >
               {title}
             </Link>
