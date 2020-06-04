43dad58e345f1222fb4225332e2619c5a798b2e7
Jonathan Steele <jonathan@steele.pro>
2020-06-04 18:09:55 +0100

    feat: #BlackLivesMatter banner (#103)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -21,6 +21,21 @@ function Layout({ children, pageContext }) {
         }}
       >
         <div className="min-h-screen">
+          <div className="bg-black">
+            <div className="max-w-3xl mx-auto p-4">
+              <p className="md:text-base text-sm text-white">
+                <strong>#BlackLivesMatter</strong>.{' '}
+                <a
+                  className="underline"
+                  href="https://secure.actblue.com/donate/ms_blm_homepage_2019"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Support the movement
+                </a>
+              </p>
+            </div>
+          </div>
           <Header />
           <main className="mx-auto md:py-8 max-w-3xl px-4 py-6">
             {children}
